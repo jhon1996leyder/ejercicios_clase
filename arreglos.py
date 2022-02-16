49ee42ad9b0616be1e575684edb79d32b4187907
jhon_1996leyder <08jhonleider@gmail.com>
2022-02-16 15:38:56 -0500

    nuevo

---
+++ arreglos.py
@@ -0,0 +1,18 @@
+"""
+numeros=[2,3,4,5,6,7,8,9]
+
+for i in range(len(numeros)):
+    print(f"{numeros[i]}+1={numeros[i]+1}")
+
+#-----------------------------------
+"""
+numeros=[1,2,3,4,5,6,7]
+suma=[]
+
+for i in range(len(numeros)):
+    suma.append(numeros[i]+1)
+    #print(f"{numeros[i]}+1={numeros[i]+1}")
+print(suma)
+for n in range(len(suma)):
+    print(suma[n])
+
