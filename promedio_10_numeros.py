4416debbcbf8c0eae329d86247a62b9c83495d28
jhon_1996leyder <08jhonleider@gmail.com>
2022-02-16 14:28:48 -0500

    en clase

---
+++ promedio_10_numeros.py
@@ -0,0 +1,8 @@
+contador=0
+n1=0
+final=10
+while n1 <=final:
+    numero=int(input("ingrese diez numeros:"))
+    contador=contador+numero
+    n1+=1  
+print(contador/final)
---
+++ tabla_de_multiplicar.py
@@ -0,0 +1,4 @@
+numero=int(input("ingrese un numero:"))
+for i in range(50):
+    print(f"{numero} x {i} = {i*numero}")
+
